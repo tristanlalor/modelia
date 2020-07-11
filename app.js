a1724c091ae919aef8614b37aac86c0337855f03
tristanlalor <59893015+tristanlalor@users.noreply.github.com>
2020-07-11 01:52:37 -0400

    app.js

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
 // app.set('views', __dirname);
 // app.set('views', path.join(__dirname, 'views'));
-app.set('views', path.join(__dirname, ''));
+// app.set('views', path.join(__dirname, ''));
+app.set('views', __dirname + '/views');
 
 //link to public
 app.use(express.static(path.join(__dirname, 'public')));
