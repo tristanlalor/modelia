7796db4498298c80deb31850c6ece64d3c1c0e8d
tristanlalor <59893015+tristanlalor@users.noreply.github.com>
2020-07-11 01:40:38 -0400

    ./

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const { ensureAuthenticated } = require('../config/auth')
 //Welcome page
 router.get('/', (req, res) => {
     if (req.user) {
-        res.redirect('/dashboard');
-    } else res.render('Welcome');
+        res.redirect('./dashboard');
+    } else res.render('./Welcome');
 });
 //Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard', {
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('./dashboard', {
     user: req.user
 }));
 
