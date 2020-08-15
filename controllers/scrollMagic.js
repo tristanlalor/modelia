9d8ce8766438691469ffa6a7507546016238496d
tristanlalor <59893015+tristanlalor@users.noreply.github.com>
2020-08-15 19:44:54 -0400

    tweak flexbox in summary tab

--- controllers/scrollMagic.js
+++ controllers/scrollMagic.js
@@ -1,13 +1,13 @@
 const parallax1 = document.querySelector('#abt-parallax-1');
-const parallax2 = document.querySelector('#abt-parallax-2');
+// const parallax2 = document.querySelector('#abt-parallax-2');
 const abt = document.querySelector('.abt');
 abt.addEventListener('scroll', () => {
     let scroll = abt.scrollTop;
     // const offset = abt.getBoundingClientRect().top - abt.offsetParent.getBoundingClientRect().top;
     console.log(scroll);
 
-    parallax1.style.transform = `translateY(${scroll * -0.3}px)`;
-    parallax2.style.transform = `translateY(calc(${scroll * 0.3}px - 500px))`;
+    parallax1.style.transform = `translateY(calc(${scroll * 0.3}px - 200px))`;
+    // parallax2.style.transform = `translateY(calc(${scroll * 0.3}px - 500px))`;
 });
 
 // document.querySelector('.all-pages-container').style.transform = "translate(-200vw)";
